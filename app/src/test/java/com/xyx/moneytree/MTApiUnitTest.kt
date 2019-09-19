e98c00907882c54699781e88841038974f363bee
rainshang <rainshanghsu@qq.com>
2019-09-19 16:35:03 +1000

    fucking yeah!!! The first unit test works!

--- app/src/test/java/com/xyx/moneytree/MTApiUnitTest.kt
+++ app/src/test/java/com/xyx/moneytree/MTApiUnitTest.kt
@@ -1,14 +1,16 @@
 package com.xyx.moneytree
 
-import com.xyx.moneytree.data.MTRepo
 import com.xyx.moneytree.data.api.MTApi
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
-import kotlin.reflect.full.declaredMembers
-import kotlin.reflect.full.memberProperties
 
 
 class MTApiUnitTest {
@@ -17,6 +19,7 @@ class MTApiUnitTest {
     @Before
     fun setup() {
         server = MockWebServer()
+        server.start()
     }
 
     @After
@@ -26,9 +29,19 @@ class MTApiUnitTest {
 
     @Test
     fun getAccountsTest() {
-        val uri = javaClass.classLoader!!.getResource("accounts.json")
-        val file = File(uri.path)
-        println(String(file.readBytes()))
+        server.enqueue(MockResponse().apply {
+            val uri = javaClass.classLoader!!.getResource("accounts.json")
+            val file = File(uri.path)
+            setBody(String(file.readBytes()))
+        })
+
+        val mtApi = Retrofit.Builder()
+            .baseUrl(server.url(""))
+            .addConverterFactory(GsonConverterFactory.create()).build()
+            .create(MTApi::class.java)
+        val accountWrapper = mtApi.getAccounts(0).execute().body()
+        assertNotNull(accountWrapper!!.data)
+        assertEquals(accountWrapper.data.size, 3)
     }
 
 }
